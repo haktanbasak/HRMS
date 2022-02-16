ef768cbe853bef27c106a4ea3a77de61acb95c3a
Haktan Başak <haktanbasak9@gmail.com>
2022-02-16 10:59:36 +0300

    Added and listed employers

--- hrms/src/main/java/kodlama/io/hrms/business/abstracts/EmployerService.java
+++ hrms/src/main/java/kodlama/io/hrms/business/abstracts/EmployerService.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import kodlama.io.hrms.core.utilities.results.DataResult;
 import kodlama.io.hrms.core.utilities.results.Result;
-import kodlama.io.hrms.entities.concretes.JobPosition;
+import kodlama.io.hrms.entities.concretes.Employer;
 
-public interface JobPositionService {
-	DataResult<List<JobPosition>> getAll();
+public interface EmployerService {
+	DataResult<List<Employer>> getAll();
 	
-	Result add(JobPosition jobPosition);
+	Result add(Employer employer);
 }
